9b830cf7cbb2eb3d0231b066ad2757c74d5da321
akash jha <akashjhasciencea@gmail.com>
2024-04-30 19:10:07 +0530

    added remove button and some fixes project complete

--- new.js
+++ new.js
@@ -1,26 +1,39 @@
-
+let n = 0;
 let myLib = [];
-function objec(name ,author, pages){
+function objec(name ,author, pages,id){
     this.name = name;
     this.author = author;
     this.pages = pages
+    this.id = id;
+
 }
-function newbook(name ,author, pages){
-    let newbook = new objec(name ,author, pages);
+
+function newbook(name ,author, pages, id){
+    let newbook = new objec(name ,author, pages, id);
+    newbook.remove = function(x){
+        let ind = x;
+        myLib.splice(ind,1);
+        rm();
+        arrLoop();
+        n = myLib.length;
+    }
     myLib.push(newbook);
 }
+
 document.getElementById("add").addEventListener("click", ()=>{
     document.getElementById("form-div").style.display = "block";
 })
+
 document.getElementById("form").addEventListener("submit", (e)=>{
+    n += 1;
     e.preventDefault();
     let form = document.getElementById("form");
     const name = form.name.value;
     const author = form.author.value;
     const pages = form.pages.value;
-    newbook(name, author, pages);
+    newbook(name, author, pages, n);
     console.log(name + author + pages);
-    form.display="none";
+    document.getElementById("form-div").style.display="none";
     rm()
    arrLoop();
 })
@@ -30,7 +43,7 @@ function arrLoop(){
         let child = document.createElement("div");
         child.className = "item";
         child.id = i ;
-        child.innerHTML = `name: ${myLib[i].name} author: ${myLib[i].author} pages: ${myLib[i].pages}`
+        child.innerHTML = `name: ${myLib[i].name} <br> author: ${myLib[i].author} <br> pages: ${myLib[i].pages} <br> id = ${myLib[i].id} <br><br> <button onclick="myLib[${i}].remove(${i})" class="btn btn-info">remove</button>`
         grid.appendChild(child);
         myLib[i].index = i;
     }
