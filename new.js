2bf4bd7c40aab0216283331812bfc264d147eede
Akash Jha <akashjhasciencea@gmail.com>
2024-05-03 22:45:18 +0530

    Update new.js
    wrote comments

--- new.js
+++ new.js
@@ -1,6 +1,7 @@
+//I AAM VIOLENTLY DRRUNK AND IDK WHAT TO DO SO IM WRITING COMMENTS
 let n = 0;
 let myLib = [];
-function objec(name ,author, pages,id){
+function object_constructor(name ,author, pages,id){  //constructor of a book
     this.name = name;
     this.author = author;
     this.pages = pages
@@ -9,7 +10,7 @@ function objec(name ,author, pages,id){
 }
 
 function newbook(name ,author, pages, id){
-    let newbook = new objec(name ,author, pages, id);
+    let newbook = new object_constructor(name ,author, pages, id);
     newbook.remove = function(x){
         let ind = x;
         myLib.splice(ind,1);
@@ -28,16 +29,16 @@ document.getElementById("form").addEventListener("submit", (e)=>{
     n += 1;
     e.preventDefault();
     let form = document.getElementById("form");
-    const name = form.name.value;
+    const name = form.name.value;  
     const author = form.author.value;
     const pages = form.pages.value;
-    newbook(name, author, pages, n);
+    newbook(name, author, pages, n);  //creates a book object and pushes it into myLib  array
     console.log(name + author + pages);
     document.getElementById("form-div").style.display="none";
-    rm()
-   arrLoop();
+    rm()  //remove all old items
+   arrLoop();  //add new array of boos into grid
 })
-function arrLoop(){
+function arrLoop(){    //redraw all array items into grid
     for(i = 0; i < myLib.length; i++){
         let grid = document.getElementById("grid");
         let child = document.createElement("div");
@@ -48,6 +49,6 @@ function arrLoop(){
         myLib[i].index = i;
     }
 }
-function rm(){
+function rm(){   //remove all grid items
     document.getElementById("grid").innerHTML = "";
 }
